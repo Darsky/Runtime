726aafc2c37cb19438b6bf41b329951abef9fc57
Darsky <276294393@qq.com>
2018-01-03 15:41:36 +0800

    add Resource Code

---
+++ Runtime/Runtime/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Runtime
+//
+//  Created by Darsky on 2018/1/3.
+//  Copyright © 2018年 Darsky. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Runtime/Runtime/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Runtime
+//
+//  Created by Darsky on 2018/1/3.
+//  Copyright © 2018年 Darsky. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
